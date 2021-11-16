20c008d4e9374c241c2014f07a7784746b44735f
jpls19 <94463836+jpls19@users.noreply.github.com>
2021-11-16 16:14:28 +0000

    Add files via upload

--- ex6_2.cpp
+++ ex6_2.cpp
@@ -5,6 +5,8 @@
 #include <ctype.h>
 #include <iostream>
 #include "AudioFile/AudioFile.h"
+#include <math.h>  
+
 
 using namespace std;
 
@@ -20,20 +22,18 @@ double check_key(map<double, long int> m, double key)				// função para verifi
 int main(int argc, char* argv[]){
 
   AudioFile<double> audioFile;
-  AudioFile<double> audioFileOut;
 
   audioFile.load("sample02.wav");   //mudar para argv[1]
 
-      if (argc==1)	{													// indicação da sintaxe de commando
-		cout << "Usage: ./program_name ./original_audio_file_name.wav" << endl;
-	}
-	else {
+     // if (argc==1)	{													// indicação da sintaxe de commando
+	//	cout << "Usage: ./program_name ./original_audio_file_name.wav" << endl;
+	//}
+	//else {
 
 
-	if (!ifs.is_open()) {								                    // avisa se o ficheiro de texto não for encontrado ("/a" faz emitir um beep)
-		cout << "\a" << "failed to open file \"" << argv[1] << "\"" << endl;
-	}
-	else {
+	//if (!audioFile.is_open()) {								                    // avisa se o ficheiro de texto não for encontrado ("/a" faz emitir um beep)
+	//	cout << "\a" << "failed to open file \"" << argv[1] << "\"" << endl;
+	//}
 
  // audioFileOut.setNumChannels(audioFile.getNumChannels());
  // audioFileOut.setNumSamplesPerChannel(audioFile.getNumSamplesPerChannel());
@@ -48,36 +48,63 @@ int main(int argc, char* argv[]){
     cout << "bitDepth = " << bitDepth << endl;
 
 
- ofstream ofs("channel1.txt");
+ofstream ofs1("channel1.txt");
+ofstream ofs2("channel2.txt");
+ofstream ofs3("mono.txt");
 
 
-    map<double,long int> samp;
+    map<double,long int> samp1;
+    map<double,long int> samp2;
+    map<double,long int> samp3;
+
 
         double x;
 
-        for (int c = 0; c < 1; c++){
-            for (long int i = 0; i < 10000; i++){
+        for (int c = 0; c < 2; c++){
+            for (long int i = 0; i < numSamples; i++){
 
-            double x = (audioFile.samples[c][i])*32767;
-		int turnOffK(int x, int k)                          //coloca a 0 o bit n.º k a contar da direita de n
-            	int k = 1
-
-            if (check_key(samp,x) == '1') {		// substitui os caracteres (letras) que não estão em lowercase por lowercase
-			samp[x]++;
-            }
-            else {
-            samp.insert ( {x,1} );
-
+            double x = trunc((audioFile.samples[c][i])*32767000);
+			if (c==0) {
+				if (check_key(samp1,x) == '1') {		
+				samp1[x]++;
+				}
+				else {
+				samp1.insert ( {x,1} );
+				}}
+			else if (c==1) {
+				if (check_key(samp2,x) == '1') {		
+				samp2[x]++;
+				}
+				else {
+				samp2.insert ( {x,1} );
+				}}
+				
 //          cout << "sample = " << x << endl;
-            }
-
+            }}
+         for (auto const &pair: samp1) {
+			samp3.insert( {pair.first, pair.second});
     }
-
+    for (auto const &pair: samp2) {
+			if (check_key(samp2,pair.first) == '1') {
+				samp3[pair.first] = samp3[pair.first] + samp2[pair.first];
+			}
+			else {
+				samp3.insert( {pair.first, pair.second});
+}}
+	for (auto const &pair: samp3) {
+			samp3[pair.first]=samp3[pair.first]/2;
       // audioFileOut.samples[c][i] = audioFile.samples[c][i];
+}
+		
 
-
-     for (auto const &pair: samp) {
-        ofs << pair.first << " " << pair.second << endl;
+    for (auto const &pair: samp1) {
+        ofs1 << pair.first << " " << pair.second << endl;
+    }
+	for (auto const &pair: samp2) {
+        ofs2 << pair.first << " " << pair.second << endl;
+    }
+    for (auto const &pair: samp3) {
+        ofs3 << pair.first << " " << pair.second << endl;
     }
 
       // audioFileOut.samples[c][i] = audioFile.samples[c][i];
@@ -86,4 +113,4 @@ int main(int argc, char* argv[]){
  //// audioFile.save (argv[2], AudioFileFormat::Wave);
 
   return 0;
-}}
+}
